54181ebae3c4a328d2bc61e4ae4cb6b4674dab44
jaimelazich <jaimelazich@gmail.com>
2016-03-24 23:47:35 -0400

    database connection

--- php-queer-playlist-project/playlist.php
+++ php-queer-playlist-project/playlist.php
@@ -9,6 +9,25 @@ $section = "playlist";
 
 				<h1><?php echo $pageTitle; ?></h1>
   <?php include("inc/header.php"); ?>
+        <?php
+        try {
+            $pdo = new PDO('mysql:host=localhost;port=3306;dbname=sys', 'root', 'azul');
+              foreach($pdo->query('SELECT * from playlist_queer') as $row) {
+                    print_r($row);
+                }
+            sleep(5);
+            $stmt = $pdo->prepare('SELECT * FROM playlist_queer');
+            $stmt->execute();
+            $pdo->query('SELECT pg_terminate_backend(pg_backend_pid());');
+            $pdo = null;
+            sleep(60);
+            
+          
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+        ?>
         </div>
 
 		</div>
