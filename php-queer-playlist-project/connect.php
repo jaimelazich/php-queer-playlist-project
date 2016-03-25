8e4e5639ffa391f91c2e7c82351a16b128269ef5
Jaime Lazich <jaimelazich@gmail.com>
2016-03-25 09:25:04 -0400

    Delete connect.php

--- php-queer-playlist-project/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pdo = new PDO('mysql: host=localhost;dbname=sys;port=3306','root','azul');
-sleep(5);
-$stmt = $pdo->prepare('SELECT * FROM playlist_queer');
-$stmt->execute();
-$pdo->query('SELECT pg_terminate_backend(pg_backend_pid());');
-$pdo = null;
-sleep(60);
-?>
